595b59840814f09ca4b70d1ebce0f4cf7246e52a
Cormac Madden <maddencpm@gmailcom>
2021-05-31 00:27:16 +0100

    start unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,25 +1,45 @@
-import { TestBed } from '@angular/core/testing';
+import { Spectator, createComponentFactory } from '@ngneat/spectator/jest';
+
 import { AppComponent } from './app.component';
+import { NewsStoriesComponent } from '@components/news-stories/news-stories.component';
+
+import { MockComponent } from 'ng-mocks';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+  let spectator: Spectator<AppComponent>;
+  const createComponent = createComponentFactory({
+    component: AppComponent,
+    declarations: [
+      MockComponent(NewsStoriesComponent)
+    ]
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  beforeEach(() => spectator = createComponent());
+
+  it('should set up the screen with the correct titles and main wrapper', () => {
+    const h2Tags: Array<HTMLElement> = spectator.queryAll('h2');
+
+    const mainWrapper: HTMLElement | null = spectator.query('div');
+
+    expect(h2Tags[0]).toHaveText('Top Stories');
+    expect(h2Tags[1]).toHaveText('Best Stories');
+    expect(h2Tags[2]).toHaveText('Recent Stories');
+
+    expect(spectator.query('h1')).toHaveText('Hacker News');
+
+    expect(mainWrapper).toHaveClass('container');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('genesys app is running!');
+  it('should set the correct attribute values on the news-stories child components', () => {
+    const newsStoriesComponents: Array<HTMLElement> = spectator.queryAll('news-stories');
+
+    expect(newsStoriesComponents[0]).toHaveAttribute('colClasses', 'col-sm-12 col-md-6 col-lg-3');
+    expect(newsStoriesComponents[0]).toHaveAttribute('newsType', 'topstories');
+
+    expect(newsStoriesComponents[1]).toHaveAttribute('colClasses', 'col-sm-12 col-md-6 col-lg-3');
+    expect(newsStoriesComponents[1]).toHaveAttribute('newsType', 'beststories');
+
+    expect(newsStoriesComponents[2]).toHaveAttribute('colClasses', 'col-12');
+    expect(newsStoriesComponents[2]).toHaveAttribute('newsType', 'newstories');
   });
 });
--- src/app/shared/components/news-stories/news-stories.component.spec.ts
+++ src/app/shared/components/news-stories/news-stories.component.spec.ts
@@ -1,31 +1,55 @@
-// import { TestBed } from '@angular/core/testing';
-// import { AppComponent } from './app.component';
+import { Spectator, createComponentFactory, mockProvider } from '@ngneat/spectator/jest';
 
-// describe('AppComponent', () => {
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [
-//         AppComponent
-//       ],
-//     }).compileComponents();
-//   });
+import { NewsStoriesComponent } from './news-stories.component';
+import { NewsItemComponent } from '@components/news-item/news-item.component';
+import { HackerNewsService } from '@services/hacker-news.service';
 
-//   it('should create the app', () => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     const app = fixture.componentInstance;
-//     expect(app).toBeTruthy();
-//   });
+import { MockComponent } from 'ng-mocks';
 
-//   it(`should have as title 'genesys'`, () => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     const app = fixture.componentInstance;
-//     expect(app.title).toEqual('genesys');
-//   });
+import { of } from 'rxjs';
 
-//   it('should render title', () => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     fixture.detectChanges();
-//     const compiled = fixture.nativeElement;
-//     expect(compiled.querySelector('.content span').textContent).toContain('genesys app is running!');
-//   });
-// });
+describe('NewsStoriesComponent', () => {
+  let spectator: Spectator<NewsStoriesComponent>;
+  const createComponent = createComponentFactory({
+    component: NewsStoriesComponent,
+    declarations: [
+      MockComponent(NewsItemComponent)
+    ],
+    providers: [
+      mockProvider(HackerNewsService, {
+        getStories: () => { 
+          return of([
+            {
+              by: 'user1',
+              time: 'time1',
+              title: 'title1',
+              text: 'text1',
+              url: 'url1'
+            },
+            {
+              by: 'user2',
+              time: 'time2',
+              title: 'title2',
+              text: 'text2',
+              url: 'url2'
+            }
+          ]);
+        }
+      })
+    ]
+  });
+
+  beforeEach(() => spectator = createComponent());
+
+  it('should set up the screen with the correct titles and main wrapper', () => {
+    const mainWrapper: HTMLElement | null = spectator.query('div');
+
+    expect(mainWrapper).toHaveClass('row');
+  });
+
+  it('should set the correct attribute values on the news-stories child components', () => {
+    const newsItems: Array<HTMLElement> = spectator.queryAll('news-item');
+
+    expect(newsItems.length).toEqual(2);
+  });
+});
